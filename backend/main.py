294835cc180a66b5df37ce45d778a4e8eef51da0
Luciano Cordoba <ljoecordoba@gmail.com>
2025-03-01 20:50:45 -0300

    añadiendo comentarios que explican ejecucion del main.py

--- backend/main.py
+++ backend/main.py
@@ -9,3 +9,9 @@ async def root():
 @app.get("/url")
 async def url():
     return {"url_curso": "https://www.udemy.com/course/python-3-do-zero-ao-avancado/"}
+
+#Inicia el server: uvicorn main:app --reload
+#Detener el server: Ctrl + C
+
+#Documentacion con Swagger: http://127.0.0.1:8000/docs
+#Documentacion con ReDocly: http://127.0.0.1:8000/redoc
